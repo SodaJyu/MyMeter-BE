9e8645619a652ac1783836f77d371103970f35ba
Dominic Lowes <chaiskeng@gmail.com>
2022-08-02 15:04:44 +0900

    initial commit

---
+++ src/main/java/mymeter/BE/MymeterBeApplication.java
@@ -0,0 +1,13 @@
+package mymeter.BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymeterBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymeterBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mymeter/BE/MymeterBeApplicationTests.java
@@ -0,0 +1,13 @@
+package mymeter.BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymeterBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
