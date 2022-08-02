6d04e5adc8644c8c6c109d2ad9eb98874117d14b
Dominic Lowes <chaiskeng@gmail.com>
2022-08-02 15:05:04 +0900

    feat: add user class

--- src/main/java/mymeter/BE/User.java
+++ src/main/java/mymeter/BE/User.java
@@ -1,2 +1,24 @@
-package mymeter.BE;public class User {
+package mymeter.BE;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+public class User {
+    @Id
+    private String id;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String city;
+
+    public User(String first_name, String last_name, String email, String city) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.city = city;
+    }
+
 }
