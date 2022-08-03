d09533acf3955cbd7ee5a324829d04189c8d60a7
Dominic Lowes <chaiskeng@gmail.com>
2022-08-03 14:54:11 +0900

    chore: remove unused imports

--- src/main/java/mymeter/BE/UserRepository.java
+++ src/main/java/mymeter/BE/UserRepository.java
@@ -3,8 +3,6 @@ package mymeter.BE;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
     User findByEmail(String email);
--- src/main/java/mymeter/BE/UserService.java
+++ src/main/java/mymeter/BE/UserService.java
@@ -1,7 +1,5 @@
 package mymeter.BE;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
