a7e779df2fb6dcadbfb133138a477c4fbb13c2cc
Dominic Lowes <chaiskeng@gmail.com>
2022-08-03 17:19:59 +0900

    feat: create weather class

--- src/main/java/mymeter/BE/weather/Weather.java
+++ src/main/java/mymeter/BE/weather/Weather.java
@@ -1,2 +1,25 @@
-package mymeter.BE.weather;public class weather {
+package mymeter.BE.weather;
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Data
+@Document
+public class Weather {
+
+    public String city;
+    public Date date;
+    public String cloudCover;
+    public int windSpeed;
+    public String windDirection;
+
+    public Weather(String city, Date date, String cloudCover, int windSpeed, String windDirection) {
+        this.city = city;
+        this.date = date;
+        this.cloudCover = cloudCover;
+        this.windSpeed = windSpeed;
+        this.windDirection = windDirection;
+    }
 }
