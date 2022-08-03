61252e9e91171fd67c2f6ed81bab0f67ab30ee35
Dominic Lowes <chaiskeng@gmail.com>
2022-08-03 14:51:01 +0900

    feat: add method to ensure duplicate emails cannot be used

--- src/main/java/mymeter/BE/UserRepository.java
+++ src/main/java/mymeter/BE/UserRepository.java
@@ -3,6 +3,9 @@ package mymeter.BE;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+    User findByEmail(String email);
 }
