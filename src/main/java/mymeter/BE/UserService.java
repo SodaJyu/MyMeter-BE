50e3ee2750fa74b9f1637937765115b1ecb8f9f8
Dominic Lowes <chaiskeng@gmail.com>
2022-08-03 11:57:15 +0900

    refactor: add constructor to UserService

--- src/main/java/mymeter/BE/UserService.java
+++ src/main/java/mymeter/BE/UserService.java
@@ -1,2 +1,21 @@
-package mymeter.BE;public class UserService {
+package mymeter.BE;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserService {
+    final
+    UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public User createUser(User user) { return userRepository.save(user);}
+
+    public List<User> getUsers() { return userRepository.findAll();}
 }
