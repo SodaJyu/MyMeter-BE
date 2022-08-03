b50494e2666f1340240dec60273e09ce87c82fea
Dominic Lowes <chaiskeng@gmail.com>
2022-08-03 11:58:50 +0900

    refactor: update UserController class

--- src/main/java/mymeter/BE/UserController.java
+++ src/main/java/mymeter/BE/UserController.java
@@ -1,2 +1,28 @@
-package mymeter.BE;public class UserController {
+package mymeter.BE;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    final
+    UserService userService;
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository, UserService userService) { this.userRepository = userRepository;
+        this.userService = userService;
+    }
+
+    @PostMapping("/users")
+    public void saveUser(@RequestBody User user) { userRepository.save(user);}
+
+    @GetMapping("/users")
+    public List<User> getUsers() { return userService.getUsers();}
 }
