e048fffd499abaabda16da61e1ea768d1dba9f18
Dominic Lowes <chaiskeng@gmail.com>
2022-08-03 17:35:49 +0900

    refactor: update weather class variables

--- src/main/java/mymeter/BE/weather/Weather.java
+++ src/main/java/mymeter/BE/weather/Weather.java
@@ -12,14 +12,16 @@ public class Weather {
     public String city;
     public Date date;
     public String cloudCover;
-    public int windSpeed;
+    public int maxWindSpeed;
+    public int averageWindSpeed;
     public String windDirection;
 
-    public Weather(String city, Date date, String cloudCover, int windSpeed, String windDirection) {
+    public Weather(String city, Date date, String cloudCover, int maxWindSpeed, int averageWindSpeed, String windDirection) {
         this.city = city;
         this.date = date;
         this.cloudCover = cloudCover;
-        this.windSpeed = windSpeed;
+        this.maxWindSpeed = maxWindSpeed;
+        this.averageWindSpeed = averageWindSpeed;
         this.windDirection = windDirection;
     }
 }
