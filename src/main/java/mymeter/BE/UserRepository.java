ea8fd70211f0a0e819c50993545a708bbb27f4f8
Dominic Lowes <chaiskeng@gmail.com>
2022-08-03 11:58:12 +0900

    refactor: add repository annotation to UserRepository

---
+++ src/main/java/mymeter/BE/UserRepository.java
@@ -0,0 +1,8 @@
+package mymeter.BE;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
