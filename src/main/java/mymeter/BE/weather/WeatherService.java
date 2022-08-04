4ab0b917e0df5133efdd458e892f8f87b8aa2efc
Dominic Lowes <chaiskeng@gmail.com>
2022-08-04 11:54:51 +0900

    feat: create weather service class

--- src/main/java/mymeter/BE/weather/WeatherService.java
+++ src/main/java/mymeter/BE/weather/WeatherService.java
@@ -1,2 +1,23 @@
-package mymeter.BE.weather;public class WeatherService {
+package mymeter.BE.weather;
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class WeatherService {
+
+    WeatherRepository weatherRepository;
+
+    public WeatherService(WeatherRepository weatherRepository) {
+        this.weatherRepository = weatherRepository;
+    }
+
+    public List<Weather> getWeather() {
+        return weatherRepository.findAll();
+    }
+
+    public Weather findWeatherByCity(String city) {
+       return weatherRepository.findByCity(city);
+    }
+
 }
