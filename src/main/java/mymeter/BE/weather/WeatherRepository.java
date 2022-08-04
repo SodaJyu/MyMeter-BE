6196e3d90b5a39af377aa2d07bcc3b14ce6ed5dd
Dominic Lowes <chaiskeng@gmail.com>
2022-08-04 11:34:58 +0900

    feat: add weather repository interface

---
+++ src/main/java/mymeter/BE/weather/WeatherRepository.java
@@ -0,0 +1,8 @@
+package mymeter.BE.weather;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WeatherRepository extends MongoRepository<Weather, String> {
+}
