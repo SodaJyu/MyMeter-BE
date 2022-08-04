f27de540735d351e2c09d9bce964cbfa16f9cc51
Dominic Lowes <chaiskeng@gmail.com>
2022-08-04 11:54:20 +0900

    feat: create weather repository interface and add method to get weather by city

--- src/main/java/mymeter/BE/weather/WeatherRepository.java
+++ src/main/java/mymeter/BE/weather/WeatherRepository.java
@@ -5,4 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WeatherRepository extends MongoRepository<Weather, String> {
+    Weather findByCity(String city);
 }
