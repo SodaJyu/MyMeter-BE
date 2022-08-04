6fc39d561b18fc6c2ec3f4563d41aa382584b221
Dominic Lowes <chaiskeng@gmail.com>
2022-08-04 12:25:00 +0900

    feat: create solar repository interface

--- src/main/java/mymeter/BE/solar/SolarRepository.java
+++ src/main/java/mymeter/BE/solar/SolarRepository.java
@@ -1,2 +1,8 @@
-package mymeter.BE.solar;public interface SolarRepository {
+package mymeter.BE.solar;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SolarRepository extends MongoRepository<Solar, String> {
 }
