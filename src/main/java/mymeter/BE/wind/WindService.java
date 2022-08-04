167713d9674e1f8e6f2e2dcbc13544d8c1c45c1c
Dominic Lowes <chaiskeng@gmail.com>
2022-08-04 14:20:28 +0900

    feat: create wind service class

--- src/main/java/mymeter/BE/wind/WindService.java
+++ src/main/java/mymeter/BE/wind/WindService.java
@@ -1,2 +1,39 @@
-package mymeter.BE.wind;public class WindService {
+package mymeter.BE.wind;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class WindService {
+
+    WindRepository windRepository;
+
+    public WindService(WindRepository windRepository) {
+        this.windRepository = windRepository;
+    }
+
+    public List<Wind> getWind() {
+        return windRepository.findAll();
+    }
+
+    public Optional<Wind> getWindById(String id) {
+        return windRepository.findById(id);
+    }
+
+    public Optional<Wind> modifyWind(Wind newWind, String id) {
+        return windRepository.findById(id)
+                .map(wind -> {
+                    wind.type = newWind.getType();
+                    wind.powerRating = newWind.getPowerRating();
+                    wind.sweptArea = newWind.getSweptArea();
+                    wind.height = newWind.getHeight();
+                    return windRepository.save(wind);
+                });
+    }
+
+    public void deleteWind(String id) {
+        windRepository.deleteById(id);
+    }
 }
