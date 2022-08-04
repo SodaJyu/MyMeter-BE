ccd7217a5020c3d8a599cc36e872074b41fefd68
Dominic Lowes <chaiskeng@gmail.com>
2022-08-04 12:25:20 +0900

    feat: create solar service class

--- src/main/java/mymeter/BE/solar/SolarService.java
+++ src/main/java/mymeter/BE/solar/SolarService.java
@@ -1,2 +1,37 @@
-package mymeter.BE.solar;public class SolarService {
+package mymeter.BE.solar;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SolarService {
+
+    SolarRepository solarRepository;
+
+    public SolarService(SolarRepository solarRepository) {
+        this.solarRepository = solarRepository;
+    }
+
+    public List<Solar> getSolar() {
+        return solarRepository.findAll();
+    }
+
+    public Optional<Solar> getSolarById(String id) {
+        return solarRepository.findById(id);
+    }
+
+    public Optional<Solar> modifySolar(Solar newSolar, String id)  {
+        return solarRepository.findById(id)
+                .map(solar -> {
+                    solar.directionFacing = newSolar.getDirectionFacing();
+                    solar.kWh = newSolar.getKWh();
+                    return solarRepository.save(solar);
+                });
+    }
+
+    public void deleteSolar(String id) {
+        solarRepository.deleteById(id);
+    }
 }
