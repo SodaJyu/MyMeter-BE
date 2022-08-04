1f35ba547643d4115f6091e88811a91d3c229477
Dominic Lowes <chaiskeng@gmail.com>
2022-08-04 12:25:53 +0900

    feat: create solar controller class and routing

--- src/main/java/mymeter/BE/solar/SolarController.java
+++ src/main/java/mymeter/BE/solar/SolarController.java
@@ -1,2 +1,45 @@
-package mymeter.BE.solar;public class SolarController {
+package mymeter.BE.solar;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class SolarController {
+
+    final SolarService solarService;
+
+    private final SolarRepository solarRepository;
+
+    public SolarController(SolarService solarService, SolarRepository solarRepository) {
+        this.solarService = solarService;
+        this.solarRepository = solarRepository;
+    }
+
+    @PostMapping("/solar")
+    public String saveSolar(Solar solar) {
+        solarRepository.save(solar);
+        return "Solar Panel Successfully Registered";
+    }
+
+    @GetMapping("/solar")
+    public List<Solar> getSolar() {
+        return solarService.getSolar();
+    }
+
+    @GetMapping("/solar/{id}")
+    public Optional<Solar> getSolarById(@PathVariable String id) {
+        return solarService.getSolarById(id);
+    }
+
+    @PutMapping("/solar/{id}")
+    public Optional<Solar> modifySolar(Solar newSolar, @PathVariable String id) {
+        return solarService.modifySolar(newSolar, id);
+    }
+
+    @DeleteMapping("/solar/{id}")
+    public void deleteSolar(@PathVariable String id) {
+        solarService.deleteSolar(id);
+    }
 }
