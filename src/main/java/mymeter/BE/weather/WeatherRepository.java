f65dfbc08bf1b94d49e4bbce1a3a0e98729b23fc
Dominic Lowes <chaiskeng@gmail.com>
2022-08-05 11:14:04 +0900

    feat: add functionality to search weather repository by date

--- src/main/java/mymeter/BE/weather/WeatherRepository.java
+++ src/main/java/mymeter/BE/weather/WeatherRepository.java
@@ -3,7 +3,10 @@ package mymeter.BE.weather;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 @Repository
 public interface WeatherRepository extends MongoRepository<Weather, String> {
     Weather findByCity(String city);
+    Weather findByDate(Date date);
 }
