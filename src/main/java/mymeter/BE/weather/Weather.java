4b2ff3baf57e90a961a5f1f16b0962dee1cd0160
Dominic Lowes <chaiskeng@gmail.com>
2022-08-23 16:58:39 +0900

    refactor: change class variables and types

--- src/main/java/mymeter/BE/weather/Weather.java
+++ src/main/java/mymeter/BE/weather/Weather.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @Data
 @Document
@@ -13,23 +13,16 @@ public class Weather {
     @Id
     public String id;
     public String city;
-    public Date date;
-    public String cloudCover;
-    public int windSpeed;
-    public int maxGustSpeed;
-    public int averageWindSpeed;
-    public String windDirection;
+    public LocalDate date;
+    public double maxGustSpeed;
+    public double averageWindSpeed;
+    public double uvIndex;
 
-    public int uvIndex;
-
-    public Weather(String city, Date date, String cloudCover, int windSpeed, int maxGustSpeed, String windDirection, int uvIndex) {
+    public Weather(String city, LocalDate date, double averageWindSpeed, double maxGustSpeed, double uvIndex) {
         this.city = city;
         this.date = date;
-        this.cloudCover = cloudCover;
-        this.windSpeed = windSpeed;
         this.maxGustSpeed = maxGustSpeed;
-        this.averageWindSpeed = windSpeed + maxGustSpeed / 2;
-        this.windDirection = windDirection;
+        this.averageWindSpeed = averageWindSpeed;
         this.uvIndex = uvIndex;
     }
 }
