630a862b8f6b032d0cea3656e407d76bec8a69e1
Dominic Lowes <chaiskeng@gmail.com>
2022-08-23 16:59:05 +0900

    refactor: change date type

--- src/main/java/mymeter/BE/weather/WeatherRepository.java
+++ src/main/java/mymeter/BE/weather/WeatherRepository.java
@@ -1,13 +1,14 @@
 package mymeter.BE.weather;
 
+import org.springframework.cglib.core.Local;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface WeatherRepository extends MongoRepository<Weather, String> {
     List<Weather> findByCity(String city);
-    Weather findByDateAndCity(Date date, String city);
+    Weather findByDateAndCity(LocalDate date, String city);
 }
